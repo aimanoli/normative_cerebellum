5d43b4ad2791afe41cd09d1bfd460cf34929618e
Felix Hoffstaedter <f.hoffstaedter@fz-juelich.de>
2023-04-25 17:25:46 +0200

    fix getting T1w

--- bootstrap_freesurfer_HCP-D.sh
+++ bootstrap_freesurfer_HCP-D.sh
@@ -100,7 +100,7 @@ git checkout -b "job-\${JOBID}"
 # re-run we want to be able to do fine-grained recomputing of individual
 # outputs. The recorded calls will have specific paths that will enable
 # recomputation outside the scope of the original Condor setup
-datalad get -s inm7-storage "inputs/${MRI_dir}/\${subid}_V1_MR/T1w/T1w_acpc_dc_restore.nii.gz"
+datalad get -s inm7-storage "inputs/${MRI_dir}/\${subid}/T1w/T1w_acpc_dc_restore.nii.gz"
 
 # the meat of the matter
 # look for T1w files in the input data for the given participant
